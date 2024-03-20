1b7c3e329d557da27cea00c384d05ef7d55eed9e
Kazllin <rm87320@estudante.fieb.edu.br>
2024-03-20 19:36:23 -0300

    Nao

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import './home.css';
+import {Link} from  'react-router-dom'
+
+function Home(){
+    return(
+         <header>
+            <a href="/" className="logo">Home Marcos</a>
+            <a href="/Usuario" className="logo">Home Marcos</a>
+         </header>
+    )
+}
+
+export default Home;
---
+++ src/pages/Usuario/index.jsx
@@ -0,0 +1,13 @@
+import './usuario.css';
+import {Link} from  'react-router-dom'
+
+function Usuario(){
+    return(
+         <header>
+            <a href="/" className="logo">Home Marcos</a>
+            <a href="/Usuario" className="logo">Home Marcos</a>
+         </header>
+    )
+}
+
+export default Usuario;
